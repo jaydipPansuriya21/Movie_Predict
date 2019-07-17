0f573f27f1b652758a8e5ab8f59dc54a9c954b89
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2019-07-17 18:57:23 +0530

    base2

--- cosine.py
+++ cosine.py
@@ -1,5 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.metrics.pairwise import cosine_similarity
 
 text = ["London Paris London","Paris Paris London"]
 
@@ -7,4 +7,7 @@ cv = CountVectorizer()
 count_metrix = cv.fit_transform(text)
 
 
-print count_metrix.toarray()
+#print count_metrix.toarray()
+similarity_score = cosine_similarity(count_metrix)
+
+print similarity_score
